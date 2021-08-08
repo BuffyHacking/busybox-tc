98fc9ff4b4fdff58c9eb45efb913d5d67cff8809
BuffyHacking <88627377+BuffyHacking@users.noreply.github.com>
2021-08-08 19:12:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo " | __ ) _   _ ___ _   _| __ )  _____  __ "
 echo " |  _ \| | | / __| | | |  _ \ / _ \ \/ / "
 echo " | |_) | |_| \__ \ |_| | |_) | (_) >  <  "
 echo " |____/ \__,_|___/\__, |____/ \___/_/\_\ "
-echo "                  |___/                  "
+echo "  By BuffyHacking |___/                  "
 
 
 tce-load -w -i nano
@@ -17,3 +17,5 @@ sudo mkdir /home/tc/html
 sudo cp index.html /home/tc/html/index.html
 
 sudo /usr/local/httpd/sbin/httpd -p 80 -h /home/tc/html
+
+echo "Done!"
