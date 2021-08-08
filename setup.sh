3cf1d96c4a6758e905681dbfdc85aee04e90d563
BuffyHacking <88627377+BuffyHacking@users.noreply.github.com>
2021-08-08 19:03:09 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "  ____                  ____             "
+echo " | __ ) _   _ ___ _   _| __ )  _____  __ "
+echo " |  _ \| | | / __| | | |  _ \ / _ \ \/ / "
+echo " | |_) | |_| \__ \ |_| | |_) | (_) >  <  "
+echo " |____/ \__,_|___/\__, |____/ \___/_/\_\ "
+echo "                  |___/                  "
+
+
+tce-load -w -i nano
+
+tce-load -w -i busybox-httpd.tcz
+
+sudo mkdir /home/tc/html
+
+sudo cp index.html /home/tc/html/index.html
+
+sudo /usr/local/httpd/sbin/httpd -p 80 -h /home/tc/html
